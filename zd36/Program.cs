52aa675bfe9ccbf7c60994f319e3f6b5fae6bccc
Mikebulatov <mikebulatov@yandex.ru>
2023-08-12 13:26:04 +0700

    Задача 36. Изменение 1.

--- zd36/Program.cs
+++ zd36/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
 Console.WriteLine("Задача 36. Программа находит сумму элементов, стоящих на нечётных позициях одномерного массива, заполненный случайными числами.");
 Console.Write("Введите колличесво элементов маассива:");
@@ -7,15 +8,13 @@ int [] mass=new int [Convert.ToInt32(Console.ReadLine())];
 Console.Write("Сформированный массив:");
 for(int i=0; i<mass.Length; i++)
 {
-    mass[i]= new Random().Next();
+    mass[i]= new Random().Next(0,10);
     Console.Write(mass[i]);
     Console.Write(" ");
 }
 
 Console.WriteLine();
 int CountEven =0;
-for(int i=0; i<mass.Length; i++)
-{
-if (Even(mass[i]))CountEven++;
-}
-Console.WriteLine($"Число четных элеменов массива:{CountEven}");
+for(int i=1; i<mass.Length; i=i+2) CountEven=CountEven+mass[i];
+
+Console.WriteLine($"Сумма элеменов массива стоящих на четных позициях:{CountEven}");
